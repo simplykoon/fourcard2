a22f8c876a7c7e181f9a8adb1a5a3a4ee06b63fe
simplykoon <65523005+simplykoon@users.noreply.github.com>
2020-07-04 12:26:01 +0100

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,19 @@
+function changeColor(color) {
+	document.body.style.background = color; 
+	}
+var x = document.getElementsByTagName('h3');
+var y = document.getElementsByClassName('circle');	
+		var a = document.getElementsByClassName('super');	
+		var b = document.getElementsByClassName('team');	
+		var c = document.getElementsByClassName('karma');	
+		var d = document.getElementsByClassName('calc');	
+
+function darkmode() { 
+				x[0].style.color = 'hsl(0, 0%, 98%)';
+				changeColor ('hsl(234, 12%, 34%)');
+				y[0].style.left = '18px';
+				a[0].style.backgroundColor = 'black';
+				b[0].style.backgroundColor = 'black';
+				c[0].style.backgroundColor = 'black';
+				d[0].style.backgroundColor = 'black';
+				}
